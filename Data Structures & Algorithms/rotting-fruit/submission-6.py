d106ae54522794ee7aa1f1f2b5967176eb8e7e62
smahorker <114127699+smahorker@users.noreply.github.com>
2026-09-22 22:15:04 -0700

    Add: rotting-fruit - submission-6

---
+++ Data Structures & Algorithms/rotting-fruit/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        q = deque()
+        time, fresh = 0, 0
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh+= 1
+        
+        while q and fresh:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                    nr, nc = r + dr, c + dc
+                    if (nr in range(len(grid)) and (nc in range(len(grid[0]))) and grid[nr][nc] == 1):
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        q.append((nr, nc))
+            
+            time += 1
+        
+        return time if fresh == 0 else -1
